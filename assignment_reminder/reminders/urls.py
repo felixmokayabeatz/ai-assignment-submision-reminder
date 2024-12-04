313ccda3fc7a1f3811a72259aeeea3fd8ead4ac6
felixmokayabeatz <130485404+felixmokayabeatz@users.noreply.github.com>
2024-12-04 12:20:37 +0300

    Added the home page

--- assignment_reminder/reminders/urls.py
+++ assignment_reminder/reminders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import send_reminders
+from .home import home
 
 urlpatterns = [
+    path('', home, name='land_page'),
     path('predict/', send_reminders.predict_submission, name='predict_submission'),
 ]
