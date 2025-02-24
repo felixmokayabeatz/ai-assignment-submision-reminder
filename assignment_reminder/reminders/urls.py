23764544d49c791e006486a332919ef3511bc418
felixmokayabeatz <130485404+felixmokayabeatz@users.noreply.github.com>
2025-02-24 12:31:45 +0300

    Update the urls

--- assignment_reminder/reminders/urls.py
+++ assignment_reminder/reminders/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 # from . import send_reminders
 from .home import home
+# from .views import submit_assignment
+from .views import assignment_list, submit_assignment, submit_assignment
 
 urlpatterns = [
-    path('', home, name='land_page'),
+    # path('', home, name='land_page'),
     # path('predict/', send_reminders.predict_submission, name='predict_submission'),
+    # path("", assignment_list, name="assignment_list"),
+    path("assignments/", assignment_list, name="assignment_list"),
+     
+     
+    path("submit/<int:assignment_id>/", submit_assignment, name="submit_assignment"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
