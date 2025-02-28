fea153944c50640809a29b10958daaba9b00268f
felixmokayabeatz <130485404+felixmokayabeatz@users.noreply.github.com>
2025-02-28 20:36:07 +0300

    Remove urls

--- assignment_reminder/reminders/urls.py
+++ assignment_reminder/reminders/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path, include
-# from . import send_reminders
-# from .views import submit_assignment
 from .views import assignment_list, submit_assignment, submit_assignment, home
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', home, name='home'),
-    # path('predict/', send_reminders.predict_submission, name='predict_submission'),
-    # path("", assignment_list, name="assignment_list"),
     path("assignments/", assignment_list, name="assignment_list"),
      
      
