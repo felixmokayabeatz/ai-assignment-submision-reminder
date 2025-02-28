8d4aac10aa8aba17f0726f66ecb89d6b20dd049d
felixmokayabeatz <130485404+felixmokayabeatz@users.noreply.github.com>
2025-02-28 20:27:12 +0300

    Update urls

--- assignment_reminder/reminders/urls.py
+++ assignment_reminder/reminders/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 # from . import send_reminders
 # from .views import submit_assignment
 from .views import assignment_list, submit_assignment, submit_assignment, home
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', home, name='home'),
@@ -11,5 +12,6 @@ urlpatterns = [
      
      
     path("submit/<int:assignment_id>/", submit_assignment, name="submit_assignment"),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
