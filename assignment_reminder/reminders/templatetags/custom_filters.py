c68ab8ef524481cbfd8b054224abd59315ef1cc3
felixmokayabeatz <130485404+felixmokayabeatz@users.noreply.github.com>
2025-03-04 16:06:37 +0300

    Refactor custom_filters.py to improve dictionary value retrieval functions and enhance code clarity

--- assignment_reminder/reminders/templatetags/custom_filters.py
+++ assignment_reminder/reminders/templatetags/custom_filters.py
@@ -1,9 +1,28 @@
+# from django import template
+
+# register = template.Library()
+
+# @register.filter
+# def dict_get(dictionary, key):
+#     """Custom template filter to get a dictionary value by key."""
+#     return dictionary.get(key, None)
+
+
+# from django import template
+
 from django import template
 
 register = template.Library()
+register = template.Library()
 
 @register.filter
-def dict_key(d, key):
-    """Retrieve a value from a dictionary by key."""
-    return d.get(key, None)
+def dict_get(dictionary, key):
+    """Returns the value from a dictionary for the given key."""
+    return dictionary.get(key)
 
+
+
+@register.filter
+def dict_key(dictionary, key):
+    """Retrieve a value from a dictionary using a key in Django templates."""
+    return dictionary.get(key)
