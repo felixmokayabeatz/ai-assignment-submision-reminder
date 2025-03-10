7000d4952d7d1482cdecfbe1e15c0d169f9f4b77
felixmokayabeatz <130485404+felixmokayabeatz@users.noreply.github.com>
2025-03-10 11:51:34 +0300

    Add enroll course

--- assignment_reminder/reminders/urls.py
+++ assignment_reminder/reminders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import assignment_list, submit_assignment, submit_assignment, home, enroll
+from .views import assignment_list, submit_assignment, submit_assignment, home, enroll, enroll_course
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', home, name='home'),
     path("assignments/", assignment_list, name="assignment_list"),
+    path("enroll-course/", enroll_course, name="enroll-course"),
     path('enroll/<int:course_id>/', enroll, name='enroll'),
      
     path("submit/<int:assignment_id>/", submit_assignment, name="submit_assignment"),
