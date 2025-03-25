3d3f07f9e7682a7d13c751a83844e2d7b3c11a99
felixmokayabeatz <130485404+felixmokayabeatz@users.noreply.github.com>
2025-03-25 10:51:49 +0300

    Fix typo in attachment field definition in Assignment model

--- assignment_reminder/submissions/models.py
+++ assignment_reminder/submissions/models.py
@@ -14,7 +14,7 @@ class Assignment(models.Model):
     description = models.TextField()
     deadline = models.DateTimeField()
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
-    attachment = models.FileField(upload_to='assignments/', null=True, blansk=True)
+    attachment = models.FileField(upload_to='assignments/', null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if self.deadline and self.deadline.tzinfo is None:
